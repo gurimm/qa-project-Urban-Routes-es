5c2322b7214a9274c32af5d34e8e145f0b1ba3ae
gurimm <gurimunoz@gmail.com>
2025-01-24 23:17:06 -0600

    AcSe agregan los assets a cada prueba y se incluye un selector adicional CSS

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-0e942b1d-31fa-4f95-a7cf-6a2e8bbc09e7.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-8e1dae64-2c36-44df-8e74-56b30fafc158.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
